0897f949814d0270778043bc0bab9ea4c6e1af14
Dhruv Patel <dhruvp.dev@gmail.com>
2024-08-31 16:46:25 +0530

    file handle using multer and cloudinary

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,33 @@
+import fs from "node:fs";
+import { v2 as cloudinary } from 'cloudinary';
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dzx3ww3jf', 
+        api_key: process.env.CLOUDNARY_API_KEY, 
+        api_secret:process.env.CLOUDNARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    
+  
+    const uploadOnCloudnary = async (localFilePath) =>{
+
+        try {
+            if(!localFilePath) return null;
+
+            const cloudnaryResponse = await cloudinary.uploader.upload(localFilePath,{
+                resource_type:"auto"
+            })
+
+            return cloudnaryResponse
+            
+        } catch (error) {
+
+            fs.unlinkSync(localFilePath);
+
+            return null;
+            
+        }
+    }
+
+
+    export {uploadOnCloudnary}
